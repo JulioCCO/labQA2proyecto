aaa4af7c011cc482741e034fe489b27b25c04ff9
Francisco Ovares Rojas <82686857+fran1709@users.noreply.github.com>
2023-10-21 15:13:22 -0600

    casos_prueba_desconnect
    Prueba si desconecta un usuario, con ambos casos true/false.

--- src/app/laboratorio2/login-service.spec.ts
+++ src/app/laboratorio2/login-service.spec.ts
@@ -1,10 +1,61 @@
 import { LoginService } from './login-service';
 import { AccountRepository } from './account-repository';
-
+import { instance, mock, reset, when } from 'ts-mockito';
+import { Account } from './account';
+/*
 describe('LoginService', () => {
   let ac:AccountRepository;
   it('should create an instance', () => {
     
     expect(new LoginService(ac)).toBeTruthy();
+  });
+});*/
+
+describe('LoginService - desconnect', () => {
+  let loginService: LoginService;
+  let accountRepository: AccountRepository;
+  let accountRepositoryMock: AccountRepository;
+
+  beforeEach(() => {
+    // Crea un doble de prueba para AccountRepository
+    accountRepositoryMock = mock<AccountRepository>();
+    accountRepository = instance(accountRepositoryMock);
+
+    // Inicializa el servicio con el doble de prueba
+    loginService = new LoginService(accountRepository);
+  });
+
+  afterEach(() => {
+    // Restablece el comportamiento de los dobles de prueba después de cada prueba
+    reset(accountRepositoryMock);
+  });
+
+  it('Se desconecta el usuario (TRUE)', () => {
+    // Arrange
+    const userToDisconnect = new Account();
+    userToDisconnect.setUser('usuario');
+  
+    // Configura el doble de prueba para devolver una lista que contiene la cuenta a desconectar.
+    when(accountRepositoryMock.getListaConnects()).thenReturn([userToDisconnect]);
+  
+    // Act
+    const result = loginService.desconnect('usuario');
+  
+    // Assert
+    expect(result).toBe(true, 'Debería devolver verdadero');
+  });
+  
+
+  //debería devolver falso si el usuario no está conectado
+  it('Se esconecta el usuario (FALSE)', () => {
+    // Arrange
+    // Configura el doble de prueba para devolver una lista vacía, simulando que el usuario no está conectado.
+    when(accountRepositoryMock.getListaConnects()).thenReturn([]);
+
+    // Act
+    const result = loginService.desconnect('usuario');
+
+    // Assert
+    expect(result).toBe(false, 'Debería devolver falso ya que el usuario no estaba conectado');
   });
 });
